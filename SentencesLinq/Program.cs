3ec73bcbdb5c48e8237ad3d1f3dec0e15e606234
Yuriy <yuriy.paslavsky@gmail.com>
2019-06-19 01:29:47 +0300

    Working with linq

---
+++ SentencesLinq/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentencesLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SentenceMaster master = new SentenceMaster();
+            var sentences = SentenceMaster.GetSentences();
+
+            //Calculate of the word count of each sentences
+            foreach (var item in sentences)
+            {
+                Console.WriteLine(master.GetWordsCountInSentence(item)); //delegate create mb
+            }
+            Console.WriteLine(new string('*',40));
+
+            //Split the sentences into an array of words and select the ones that start with a vowel
+            foreach (var item in sentences)
+            {
+                master.PrintWordStartsWithVowel(master.SplitSentenceIntoArray(item));
+            }
+            Console.WriteLine(new string('*', 40));
+
+            //Find the longest word
+            Console.WriteLine(master.GetLongestWord(sentences));
+
+            //Display the average word count of the sentences
+            Console.WriteLine(master.AverageWordCount(sentences));
+           
+            
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ SentencesLinq/SentenceMaster.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SentencesLinq
+{
+    public class SentenceMaster
+    {
+        public string LongestWord { get; set; } = String.Empty;
+
+        public static string[] GetSentences()
+        {
+            string[] strings =
+             {
+                "You only live forever in the lights you make",
+                "When we were young we used to say",
+                "That you only hear the music when your heart begins to break",
+                "Now we are the kids from yesterday"
+            };
+
+            return strings;
+        }
+
+        public int GetWordsCountInSentence(string item)
+        {
+            int wordsInSentence = item.Split(' ').Where(s => !string.IsNullOrEmpty(s)).Count();
+
+            //Console.WriteLine(wordsInSentence);
+            return wordsInSentence;
+
+        }
+
+        public string[] SplitSentenceIntoArray(string item)
+        {
+            return item.Split(' ').Select(w => w).ToArray();
+        }
+
+        public void PrintWordStartsWithVowel(string[] words)
+        {
+            string[] vowels = { "a", "e", "i", "o", "u" };
+            var selectedWords = words.Where(w => vowels.Any(e => w.StartsWith(e)));
+
+            selectedWords.ToList().ForEach(i => Console.WriteLine(i));
+        }
+
+
+        public string GetLongestWord(string[] sentences)
+        {
+            string longestWord;
+
+            foreach (var item in sentences)
+            {
+                longestWord = SplitSentenceIntoArray(item).OrderByDescending(w => w.Length).First();
+
+                if (LongestWord.Length < longestWord.Length)
+                {
+                    LongestWord = longestWord;
+                }
+
+            }
+            return LongestWord;
+        }
+
+        public int AverageWordCount(string[] sentences)
+        {
+            int totalCount = default(Int32);
+
+            sentences.ToList().ForEach(i => totalCount += GetWordsCountInSentence(i));
+
+            return totalCount / sentences.Count();
+        }
+
+    }
+}
+
